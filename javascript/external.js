97278350cebe83b9f49179dbae01120637ad76a5
Jordan Shute <lpv25@txstate.edu>
2026-09-15 18:49:48 -0500

    Completed Task 4

--- javascript/external.js
+++ javascript/external.js
@@ -7,5 +7,12 @@
 // const y = "mapping";
 // const out= x + y;
 // document.write(out);
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+a = 22;
+const b =33;
+document.writeln(a+b);
+document.writeln("<br>");
+document.writeln(a += b);
+document.writeln("<br>");
+document.writeln(a += b);

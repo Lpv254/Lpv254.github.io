659f2934eb5f40ec34d97c8c8183c2ea279d5e4a
Jordan Shute <lpv25@txstate.edu>
2026-09-16 16:19:55 -0500

    Completed Lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,6 +1,7 @@
 let userName = prompt('What is your name?')
 document.getElementById('welcome').textContent = `Hello, welcome ${userName}!`
-arr = ["Mael","Eric","Randall","James"];
+
+//arr = ["Mael","Eric","Randall","James"];
 for (var i=0; i < arr.length; i++)
 {
   document.writeln(arr[i],"<br>");

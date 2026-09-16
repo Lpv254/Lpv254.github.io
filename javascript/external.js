4baec80f1627c27637015d249e97d08ad594c424
Jordan Shute <lpv25@txstate.edu>
2026-09-16 16:13:05 -0500

    Completed Lab 3

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,7 @@
 let userName = prompt('What is your name?')
 document.getElementById('welcome').textContent = `Hello, welcome ${userName}!`
+document.writeln("My name is Jordan Shute.<br>");
+
 arr = ["Mael","Eric","Randall","James"];
 for (var i=0; i < arr.length; i++)
 {
